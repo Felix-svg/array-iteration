1b4ea40c20254bf7d3cc5bb90c22cf02a12e0371
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-20 23:05:05 +0300

    forEach and DOM manipulation

---
+++ forEach.js
@@ -0,0 +1,63 @@
+const users = [
+  {
+    firstName: "Niky",
+    lastName: "Morgan",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Jaguar",
+    personalQuote: "You're never too old to learn something new",
+  },
+  {
+    firstName: "Tracy",
+    lastName: "Lum",
+    favoriteColor: "Yellow",
+    favoriteAnimal: "Penguin",
+    personalQuote: "I just got lost in thought - it was unfamiliar territory",
+  },
+  {
+    firstName: "Josh",
+    lastName: "Rowley",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Penguin",
+    personalQuote: "Always remember you’re unique, just like everyone else",
+  },
+  {
+    firstName: "Kate",
+    lastName: "Travers",
+    favoriteColor: "Red",
+    favoriteAnimal: "Jaguar",
+    personalQuote: "Behind every great man is a woman rolling her eyes",
+  },
+  {
+    firstName: "Avidor",
+    lastName: "Turkewitz",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Penguin",
+    personalQuote:
+      "You don’t have to see the whole staircase, just take the first step",
+  },
+  {
+    firstName: "Drew",
+    lastName: "Price",
+    favoriteColor: "Yellow",
+    favoriteAnimal: "Elephant",
+    personalQuote:
+      "Failure is not the opposite of success: it’s part of success",
+  },
+];
+
+function printFirstName(collection) {
+  collection.forEach((user) => {
+    const container = document.getElementById("container");
+    const namesList = document.createElement("ul");
+    const names = document.createElement("li");
+
+    names.textContent = `${user.firstName + " " + user.lastName}`;
+    names.style.listStyle = "none";
+
+    namesList.appendChild(names);
+    container.append(names);
+
+    console.log(user.firstName);
+  });
+}
+printFirstName(users);
